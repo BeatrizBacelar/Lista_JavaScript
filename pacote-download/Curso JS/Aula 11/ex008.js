db9e46fb727563a18b68f613d49aad8136a96439
Beatriz da Cruz Bacelar <beabacelar18@gmail.com>
2020-09-17 23:25:08 -0300

    commit full list JavaScript

---
+++ pacote-download/Curso JS/Aula 11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if (vel>60){ // condição simples
+    console.log('Você ultrapassou a velociidade permitida. MULTADO!!')
+}
+console.log('Dirija sempre utilizando cinto de segurança')
+
+
+
